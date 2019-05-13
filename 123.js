b2eb47467c1e352eca6bacecc166a49ff19d5308
Cameron Wark <cameronwark7@gmail.com>
2019-05-13 13:14:05 -0700

    added working firebase code

--- 123.js
+++ 123.js
@@ -8,14 +8,14 @@
 
   
   var firebaseConfig = {
-    apiKey: "AIzaSyA5lvb0QICVZWn8nfDLiBeEAxDMDitldbY",
-    authDomain: "ship-green.firebaseapp.com",
-    databaseURL: "https://ship-green.firebaseio.com",
-    projectId: "ship-green",
-    storageBucket: "ship-green.appspot.com",
-    messagingSenderId: "1082500586647",
-    appId: "1:1082500586647:web:4e925b6ff426a908"
-  };
+  apiKey: "AIzaSyCOvdHqRz3f-5thaHgh8FxuGoBfbaGYL8M",
+  authDomain: "ship-green-ec2e6.firebaseapp.com",
+  databaseURL: "https://ship-green-ec2e6.firebaseio.com",
+  projectId: "ship-green-ec2e6",
+  storageBucket: "ship-green-ec2e6.appspot.com",
+  messagingSenderId: "98708125955",
+  appId: "1:98708125955:web:abb3048244ea3bca"
+};
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
 app_firebase = firebase;
